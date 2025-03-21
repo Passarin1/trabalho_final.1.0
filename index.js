493455e7e6037b282e080bbc8dc6e3c4243ce757
Luís Henrique <165834056+Passarin1@users.noreply.github.com>
2025-03-21 01:20:30 +0000

    oi

---
+++ index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.get("/", (req, res) => {
+  res.send("API funcionando!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log('Servidor rodando na porta ${PORT}'));
+
+
+const authRoutes = require("./routes/authRoutes");
+app.use("/auth", authRoutes);
